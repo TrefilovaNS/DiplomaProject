677a46b4ffb7dd8b42f9597e0aa8ab42b3918ff3
TrefilovaNS <mionavlika@ymail.com>
2016-02-15 17:16:37 +0400

    + added files

---
+++ app/EmpForm.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Пользователь
+ * @module EmpForm
+ */
+define(['orm', 'forms', 'ui'], function (Orm, Forms, Ui, ModuleName) {
+    function module_constructor() {
+        var self = this
+                , model = Orm.loadModel(ModuleName)
+                , form = Forms.loadForm(ModuleName, model);
+        
+        self.show = function () {
+            form.show();
+        };
+        
+        // TODO : place your code here
+        
+        model.requery(function () {
+            // TODO : place your code here
+        });
+        
+    }
+    return module_constructor;
+});
