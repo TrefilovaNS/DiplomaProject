5534e63747f47089954317d298599c0108bb24e2
TrefilovaNS <mionavlika@ymail.com>
2016-04-20 14:52:31 +0400

    Modified EmpForm

--- app/GUI/EmpForm.js
+++ app/GUI/EmpForm.js
@@ -10,22 +10,18 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
                 , form = Forms.loadForm(ModuleName, model);
         var aUserId = userId;
         var onSucsess;
-        var types = arguments[0];
-        
-        var saveCareer=[];
-        //соц
-        model.qTypesById.params.psyhotypes_id = types.psyhotypes_id;
-        model.qSocTypesById.params.socionicstypes_id = types.socionicstypes_id;
-       
-        
-        
-        function saveCallback(){
+
+        var saveCareer = [];
+
+
+
+        function saveCallback() {
             model.save();
         }
-        
+
         function onShow() {
 //            require(['Education', 'Interests', 'Skills', 'forms/box-pane', 'Career'], function (Edu, Intrst, Skll, Bp, Career) {
-            require(['Education','Career','forms/box-pane'], function (Edu, Career, Bp) {
+            require(['Education', 'Career', 'forms/box-pane'], function (Edu, Career, Bp) {
                 model.qEdu.params.human_id = model.human[0].human_id;
                 model.qEdu.requery(function () {
                     for (var i = 0; i < model.qEdu.length; i++) {
@@ -33,27 +29,25 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
                         edu.showOn(form.panel2);
                     }
                 });
-                
+
                 model.qCarrer.params.human_id = model.human[0].human_id;
-                model.qCarrer.requery(function(){
+                model.qCarrer.requery(function () {
                     for (var j = 0; j < model.qCarrer.length; j++) {
                         var career = new Career(model.qCarrer[j]);
                         saveCareer.push(career.save);
                         career.showOn(form.panel5);
                     }
                 });
-                        
+
                 form.email.data = model.human[0].contact;
                 form.email.field = "email";
                 form.phone.data = model.human[0].contact;
                 form.phone.field = "phonenumber";
                 form.socpage.data = model.human[0].contact;
                 form.socpage.field = "socialpage";
-                
-                //соц
-                
-                form.soctypes.displayList = model.qSocTypes;
-                form.soctypes.displayField = 'stype';
+
+
+
 
 
                 var pnlDiv = new Bp();
@@ -66,6 +60,11 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
                 pnlDiv2.width = 1;
                 pnlDiv2.background = Ui.Color.BLACK;
                 form.panel82.add(pnlDiv2);
+
+                if (!model.human[0].psyhotypes_id) {
+                    model.qTypes.push({});
+                    model.human[0].psyhotypes_id=model.qTypes.cursor.psyhotypes_id;              
+                }
 
 //                var intrst = new Intrst();
 //                intrst.showOn(form.panel7);
@@ -90,11 +89,7 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
 //                    model.qTypes.push({});
 //                    model.qSocTypes.push({});
 //                    model.qPsyTypes.push({});
-                      //соц
-                    if (types.socionicstypes_id) {
-                        form.soctypes.value = model.qSocTypes.find({socionicstypes_id: types.socionicstypes_id})[0];
-                        console.log(form.soctypes.value);
-                    }
+
                 }
                 onShow();
             });
@@ -102,13 +97,9 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
             form.show();
 
         };
-           //соц
-        form.soctypes.onValueChange = function () {
-                    if (form.soctypes.value) {
-                        types.socionicstypes_id = form.soctypes.value.socionicstypes_id;
-                    }
-                };
-                
+
+
+
         self.showModal = function (callback) {
             form.maximizable = true;
 //            form.undecorated = true;
@@ -145,7 +136,7 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
 
         form.button.onActionPerformed = function (event) {
             require(['Education', 'forms/box-pane'], function (Edu, Bp) {
-                
+
                 var index = model.qEdu.push({});
                 model.qEdu[index - 1].human_id = model.human[0].human_id;
                 var edu = new Edu(model.qEdu[index - 1]);
@@ -166,7 +157,7 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
 
         form.btnSkill.onActionPerformed = function (event) {
             require(['Skills'], function (Skll) {
-               
+
                 var skll = new Skll();
                 skll.showOn(form.panel6);
             });
@@ -179,10 +170,10 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
                 pnlDiv.background = Ui.Color.BLACK;
                 form.panel5.add(pnlDiv);
                 var index = model.qCarrer.push({});
-               model.qCarrer[index - 1].human_id = model.human[0].human_id;
+                model.qCarrer[index - 1].human_id = model.human[0].human_id;
                 var career = new Career(model.qCarrer[index - 1]);
                 career.showOn(form.panel5);
-                
+
 //                var career = new Career();
 //                career.showOn(form.panel5);
             });
@@ -190,9 +181,9 @@ define(['orm', 'forms', 'ui', 'invoke'], function (Orm, Forms, Ui, Invoke, Modul
         form.btnSave.onActionPerformed = function (event) {
 //             model.human.schema.human_id = 1;
             model.save();
-            for (var i = 0; i <saveCareer.length ; i++) {
-              var fn =   saveCareer[i];
-              fn();
+            for (var i = 0; i < saveCareer.length; i++) {
+                var fn = saveCareer[i];
+                fn();
             }
         };
 
